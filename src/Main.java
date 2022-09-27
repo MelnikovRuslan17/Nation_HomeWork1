55c914e0d6de9865532309c1ef824455f5d9b76f
Melnikov Ruslan <Melnikovruslan17@yandex.ru>
2022-09-27 22:11:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+
+        }
+
+        long toYoung = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+        System.out.println("Несовершеннолетние жители " + "= " + toYoung);
+
+       List<String> man = persons.stream()
+                .filter(x -> x.getAge() > 18)
+                .filter(x -> x.getAge() < 27)
+                .filter(x -> x.getSex() == Sex.Man)
+                .map(Person::getFamily)
+                .distinct() // Чтобы список не был очень большим
+                .collect(Collectors.toList());
+        System.out.println(man);
+
+        List<Person> worker = persons.stream()
+                .filter(x -> x.getAge() > 18)
+                .filter(x -> x.getAge() < 60)
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(x -> x.getSex() == Sex.Women)
+                .filter(x -> x.getAge() > 18)
+                .filter(x -> x.getAge() < 65)
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(x -> x.getSex() == Sex.Man)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println(worker);
+
+
+    }
+
+}
+
